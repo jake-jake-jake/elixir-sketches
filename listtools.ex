469f7a527042520bb15191e30dff9c0b0095d0c5
Jake Davis <jcdavis@awedge.net>
2017-06-05 22:39:18 -0400

    Simple rot13 mapping

--- listtools.ex
+++ listtools.ex
@@ -4,4 +4,16 @@ defmodule Listtools do
 	defp _max([], d), do: d
 	defp _max([h|t], d) when h > d, do: _max(t, h)
 	defp _max([h|t], d), do: _max(t, d)
+
+	def caesar(n) do
+		n |> Enum.map(&(_rot13(&1)))
+	end
+
+	defp _rot13(n) do
+		if (n + 13 < 122) do
+		  n + 13
+		else
+		  n - 13
+		end
+	end 
 end
