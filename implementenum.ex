636f3feea36566082ddebcb3c8b90ace1f709fe6
Jake Davis <jcdavis@awedge.net>
2017-06-14 20:08:14 -0400

    Implement Enum each, filter, split, take

--- implementenum.ex
+++ implementenum.ex
@@ -8,7 +8,7 @@ defmodule ImplementEnum do
 		end
 	end
 
-	def all?([], func), do: true
+	def all?([], _func), do: true
 	def all?([h|t], func) do
 		if func.(h) do
 			all?(t, func)
@@ -16,4 +16,36 @@ defmodule ImplementEnum do
 			false
 		end
 	end
+
+	def each([], _), do: nil
+	def each([h|t], func) do
+		func.(h)
+		each(t, func)
+	end
+
+	def filter(list, func, acc \\ [])
+	def filter([], _, acc), do: acc
+	def filter([h|t], func, acc) do
+		if func.(h) do
+			filter(t, func, acc ++ [h])
+		else
+			filter(t, func, acc)
+		end
+	end
+
+	def split(list, count, new \\ [])
+	def split([], _, new) do
+		{new, []}
+	end
+	def split(list, count, new) when count == 0 do
+		{new, list}
+	end
+	def split([h|t], count, new) do
+		split(t, count - 1, new ++ [h])
+	end 
+
+	def take(list, count, new \\ [])
+	def take([], _, new), do: new
+	def take(_, count, new) when count == 0, do: new
+	def take([h|t], count, new), do: take(t, count - 1, new ++ [h])
 end
