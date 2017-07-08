27627f2e6920c495c40e3d3bd58943cb5ccfd473
Jake Davis <jcdavis@awedge.net>
2017-07-08 17:08:09 -0400

    paired process

--- pairedprocess.ex
+++ pairedprocess.ex
@@ -1,11 +1,39 @@
 defmodule PairedProcess do
 	def greet do
 		receive do
-			{:ok, msg}
-			IO.puts "Received: #{msg}"
-			greet
+			{sender, msg} ->
+				wait = Enum.random(300..700)
+				Process.sleep(wait)
+				send sender, {:ok, "Received: #{msg}"}
+			greet()
 		end
 	end
+end
 
-	
+fred = spawn(PairedProcess, :greet, [])
+bettie = spawn(PairedProcess, :greet, [])
+
+
+send fred, {self(), "Fred"}
+send bettie, {self(), "Bettie"}
+
+receive do
+	{:ok, message} ->
+		IO.puts message
+	after 500 ->
+		IO.puts "The greeter has gone away."
+end
+
+receive do
+	{:ok, message} ->
+		IO.puts message
+	after 500 ->
+		IO.puts "The greeter has gone away."
+end
+
+receive do
+	{:ok, message} ->
+		IO.puts message
+	after 500 ->
+		IO.puts "The greeter has gone away."
 end
