0d3fddbfbcc1f6dde179b17f98664af20c88b140
Jake Davis <jcdavis@awedge.net>
2017-07-12 20:38:00 -0400

    More fun with parallel map

--- pmap.ex
+++ pmap.ex
@@ -6,7 +6,17 @@ defmodule Parallel do
 				spawn_link fn -> (send me, { self(), fun.(elem) }) end
 			 end)
 		|> Enum.map( fn (pid) ->
-				 receive do { ^pid, result} -> result end
+				 receive do { _pid, result} -> result end
 			 end)
 	end
 end
+
+defmodule Funcs do
+	def mult_wait(num) do
+		:timer.sleep(:rand.uniform(200))
+		num * num
+	end
+end
+random_number = fn num -> :rand.uniform(num) end
+
+Parallel.pmap(1..100, &(Funcs.mult_wait(&1)))
