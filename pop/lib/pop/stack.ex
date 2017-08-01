5e75339035fa854eb64d69e731a3593587424354
Jake Davis <jcdavis@awedge.net>
2017-07-31 22:23:35 -0400

    Wrap pop module in GenServer API niceness

--- pop/lib/pop/stack.ex
+++ pop/lib/pop/stack.ex
@@ -1,6 +1,17 @@
 defmodule Pop.Stack do
 	use GenServer
 
+	def start_link(stack) do
+		GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+	end
+
+	def pop do
+		GenServer.call __MODULE__, :pop
+	end
+
+	def push(item)do
+		GenServer.cast __MODULE__, {:push, item}
+	end
 	def handle_call(:pop, _from, [head|tail]) 	do
 		{ :reply, head, tail }
 	end
