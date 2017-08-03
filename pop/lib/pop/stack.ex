4cdf1167f2d77199b2cd6273fb25b905ff359e4b
Jake Davis <jcdavis@awedge.net>
2017-08-03 10:36:25 -0400

    Add terminate function.

--- pop/lib/pop/stack.ex
+++ pop/lib/pop/stack.ex
@@ -12,10 +12,16 @@ defmodule Pop.Stack do
 	def push(item)do
 		GenServer.cast __MODULE__, {:push, item}
 	end
+
 	def handle_call(:pop, _from, [head|tail]) 	do
 		{ :reply, head, tail }
 	end
+
 	def handle_cast({:push, item}, list) 	do
 		{ :noreply, [item|list] }
+	end
+
+	def terminate({:emptystack, []}) do
+		{:emptystack, []}
 	end
 end
